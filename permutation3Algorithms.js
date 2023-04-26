44238230c4ccef7d4691b12cf60c11d48cb3e8ac
Aristarh Gran <awebdeff@gmail.com>
2023-04-26 13:59:45 +0300

    add permutation3Algorithms

--- permutation3Algorithms.js
+++ permutation3Algorithms.js
@@ -1,3 +1,20 @@
+'use strict'
+
 // 3 алгоритма перестановок (рекурсия и итерация)
 // https://www.youtube.com/watch?v=SaUaIP6WnQg
 
+// Simple recursion
+
+function perm(arr) {
+	let result = []
+	if (arr.length === 1) return [arr]
+
+	arr.forEach(letter => {
+		let tmp = perm(arr.filter(_l => _l !== letter))
+		tmp = tmp.map(_set => [letter, ..._set])
+		result = [...result, ...tmp]
+	})
+	return result
+}
+
+console.log(perm(['a', 'b', 'c']))
